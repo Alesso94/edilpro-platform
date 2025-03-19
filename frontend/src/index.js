d9902df9be5f9fa2b6be99829b424be4b2bc3684
Alessandro <aless.pan79@gmail.com>
2025-03-19 13:35:22 +0100

    Fix: configurazione routing per Netlify SPA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { LanguageProvider } from './contexts/LanguageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter basename="/">
+    <LanguageProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </LanguageProvider>
+  </BrowserRouter>
 ); 
